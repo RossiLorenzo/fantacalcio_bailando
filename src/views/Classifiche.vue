a7b06d0a4616b8ca92439c69e1881273c9ec7684
Lorenzo Rossi <lorenzorossi@fb.com>
2022-08-24 20:35:48 +0100

    update code

--- src/views/Classifiche.vue
+++ src/views/Classifiche.vue
@@ -19,137 +19,41 @@
 
     <!-- Premio Montero -->
     <div class="col-lg-4 col-md-4 col-sm-6 mb-4">
-      <div class="card">
-        <div class="p-3 pb-0 card-header">
-          <div class="mb-1 row align-items-center">
-            <img src="https://staticg.sportskeeda.com/wp-content/uploads/2015/05/montero-red-card-1431423173.jpg" style="height:220px" />
-            <h6 class="mb-0 text-sm">Premio Montero</h6>
-            <p class="text-xs text-secondary mb-2">Squadra piu' fallosa</p>
-          </div>
-        </div>
-        <!-- https://c.tenor.com/4Zl464BlTSUAAAAM/football-haiti.gif --> 
-        <div class="pb-0 pt-2 card-body">
-          <LorenzoImageText 
-            :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + montero[0].Jersey"
-            :text="montero[0].Name" 
-            :secondary_text="montero[0].Coach"
-          />
-          <div class="mb-1 mt-3 row align-items-center">
-            <div class="table-responsive">
-              <table class="table align-items-center">
-                <thead>
-                  <th></th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i class="fas fa-square" style="color: #ffd500"></i>Gialli</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i class="fas fa-square" style="color: #cc0021"></i>Rossi</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0">TOTALE</th>
-                </thead>
-                <tbody>
-                  <tr v-for="(squadra, index) in montero" :key="index" >
-                    <td style="padding: 0rem 0.5rem !important">
-                      <LorenzoImageText 
-                      :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + squadra.Jersey"
-                      />
-                    </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.yellowCards }} </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.redCards }} </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ 2 * squadra.redCards + squadra.yellowCards }} </td>
-                  </tr>
-                  </tbody>
-                </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LorenzoSpecialPrize 
+        :dataset="montero"
+        Title="Premio Montero"
+        Subtitle="Squadra piu' indisciplinata"
+        Image="https://staticg.sportskeeda.com/wp-content/uploads/2015/05/montero-red-card-1431423173.jpg"
+        :Col1="{Name: 'Gialli', Icon: 'fa-square', Icon_Color: '#ffd500'}"
+        :Col2="{Name: 'Rossi', Icon: 'fa-square', Icon_Color: '#cc0021'}"
+        :Col3="{Name: 'TOTALE'}"
+      />
     </div>
 
     <!-- Premio Totti -->
     <div class="col-lg-4 col-md-4 col-sm-6 mb-4">
-      <div class="card">
-        <div class="p-3 pb-0 card-header">
-          <div class="mb-1 row align-items-center">
-            <img src="https://asroma2-cloudinary.corebine.com/asroma2-production/image/upload/c_fill,dpr_3.0,f_webp,g_center,q_auto,w_730/v1/asroma-uat/t88yhacnls3lrbcrensd" style="height: 220px" />
-            <h6 class="mb-0 text-sm">Premio Totti</h6>
-            <p class="text-xs text-secondary mb-2">Squadra con piu' rigori</p>
-          </div>
-        </div>
-        <!-- https://c.tenor.com/4Zl464BlTSUAAAAM/football-haiti.gif --> 
-        <div class="pb-0 pt-2 card-body">
-          <LorenzoImageText 
-            :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + totti[0].Jersey"
-            :text="totti[0].Name" 
-            :secondary_text="totti[0].Coach"
-          />
-          <div class="mb-1 mt-3 row align-items-center">
-            <div class="table-responsive">
-              <table class="table align-items-center">
-                <thead>
-                  <th></th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i class="fas fa-check" style="color: green"></i>Segnati</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i class="fas fa-ban" style="color: #cc0021"></i>Sbagliati</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0">TOTALE</th>
-                </thead>
-                <tbody>
-                  <tr v-for="(squadra, index) in totti" :key="index" >
-                    <td style="padding: 0rem 0.5rem !important">
-                      <LorenzoImageText 
-                      :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + squadra.Jersey"
-                      />
-                    </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.penaltiesScored }} </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.penaltiesNotScored }} </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.penaltiesScored - squadra.penaltiesNotScored }} </td>
-                  </tr>
-                  </tbody>
-                </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LorenzoSpecialPrize 
+        :dataset="totti"
+        Title="Premio Totti"
+        Subtitle="Squadra con piu' rigori"
+        Image="https://asroma2-cloudinary.corebine.com/asroma2-production/image/upload/c_fill,dpr_3.0,f_webp,g_center,q_auto,w_730/v1/asroma-uat/t88yhacnls3lrbcrensd"
+        :Col1="{Name: 'Segnati', Icon: 'fa-check', Icon_Color: 'green'}"
+        :Col2="{Name: 'Sbagliati', Icon: 'fa-ban', Icon_Color: '#cc0021'}"
+        :Col3="{Name: 'TOTALE'}"
+      />
     </div>
 
     <!-- Premio Muriel -->
     <div class="col-lg-4 col-md-4 col-sm-6 mb-4">
-      <div class="card">
-        <div class="p-3 pb-0 card-header">
-          <div class="mb-1 row align-items-center">
-            <img src="https://static.sky.it/images/skysport/it/calcio/serie-a/2020/06/02/atalanta-gol-panchina-muriel/muriel_getty.jpg" style="height: 220px" />
-            <h6 class="mb-0 text-sm">Premio Muriel</h6>
-            <p class="text-xs text-secondary mb-2">Squadra con piu' goal lasciati in panchina</p>
-          </div>
-        </div>
-        <!-- https://c.tenor.com/4Zl464BlTSUAAAAM/football-haiti.gif --> 
-        <div class="pb-0 pt-2 card-body">
-          <LorenzoImageText 
-            :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + muriel[0].Jersey"
-            :text="muriel[0].Name" 
-            :secondary_text="muriel[0].Coach"
-          />
-          <div class="mb-1 mt-3 row align-items-center">
-            <div class="table-responsive">
-              <table class="table align-items-center">
-                <thead>
-                  <th></th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i class="fas fa-futbol" style="color: green"></i>Titolari</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i class="fas fa-couch" style="color: #cc0021"></i>Panca</th>
-                  <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0">ERRORI</th>
-                </thead>
-                <tbody>
-                  <tr v-for="(squadra, index) in muriel" :key="index" >
-                    <td style="padding: 0rem 0.5rem !important">
-                      <LorenzoImageText 
-                      :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + squadra.Jersey"
-                      />
-                    </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.Titolari.reduce((partialSum, x) => partialSum + x, 0) }} </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.Panca.reduce((partialSum, x) => partialSum + x, 0) }} </td>
-                    <td style="padding: 0rem 0.5rem !important"> {{ squadra.Titolari.reduce((partialSum, x) => partialSum + x, 0) == 0 ? 0 : Math.round(100 * squadra.Panca.reduce((partialSum, x) => partialSum + x, 0) / (squadra.Titolari.reduce((partialSum, x) => partialSum + x, 0) + squadra.Panca.reduce((partialSum, x) => partialSum + x, 0)), 0) }}% </td>
-                  </tr>
-                  </tbody>
-                </table>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LorenzoSpecialPrize 
+        :dataset="muriel"
+        Title="Premio Muriel"
+        Subtitle="Squadra con piu' goal lasciati in panchina"
+        Image="https://static.sky.it/images/skysport/it/calcio/serie-a/2020/06/02/atalanta-gol-panchina-muriel/muriel_getty.jpg"
+        :Col1="{Name: 'Titolari', Icon: 'fa-futbol', Icon_Color: 'green'}"
+        :Col2="{Name: 'Panca', Icon: 'fa-couch', Icon_Color: '#cc0021'}"
+        :Col3="{Name: 'ERRORI'}"
+      />
     </div>
 
   </div>
@@ -164,14 +68,14 @@
   import cors_request from "@/assets/js/cors_request.js";
   import evaluate_promises from "@/assets/js/evaluate_promises.js";
 
-  import LorenzoImageText from "@/views/components/LorenzoImageText.vue";
+  import LorenzoSpecialPrize from "@/views/components/LorenzoSpecialPrize.vue";
 
 
   export default {
     name: "tables",
     components: {
       DefaultInfoCard,
-      LorenzoImageText
+      LorenzoSpecialPrize
     },
     data() {
       return {
@@ -286,10 +190,56 @@
       }
 
       // Crea datasets cosi da semplificare HTML
-      this.montero = squadre_con_statistica.sort((a, b) => { return 2 * b.redCards + b.yellowCards - 2 * a.redCards - a.yellowCards });
-      this.totti = squadre_con_statistica.sort((a, b) => { return b.penaltiesScored - b.penaltiesNotScored - a.penaltiesScored + a.penaltiesNotScored });
-      console.log(squadre_con_statistica);
-      this.muriel = squadre_con_statistica.sort((a, b) => { return b.Panca.reduce((partialSum, x) => partialSum + x, 0) - a.Panca.reduce((partialSum, x) => partialSum + x, 0)});
+      let montero = [];
+      for (let i = squadre_con_statistica.length - 1; i >= 0; i--) {
+        let s = squadre_con_statistica[i];
+        montero.push({
+          'Id': s.Id,
+          'Name': s.Name,
+          'Coach': s.Coach,
+          'Jersey': s.Jersey,
+          'Col1': s.yellowCards,
+          'Col2': s.redCards,
+          'Col3': 2 * s.redCards + s.yellowCards
+        })
+      }
+      montero.sort((a, b) => { return b.Col3 - a.Col3 });
+      this.montero = montero;
+
+      let totti = [];
+      for (let i = squadre_con_statistica.length - 1; i >= 0; i--) {
+        let s = squadre_con_statistica[i];
+        totti.push({
+          'Id': s.Id,
+          'Name': s.Name,
+          'Coach': s.Coach,
+          'Jersey': s.Jersey,
+          'Col1': s.penaltiesScored,
+          'Col2': s.penaltiesNotScored,
+          'Col3': s.penaltiesScored - s.penaltiesNotScored
+        })
+      }
+      totti.sort((a, b) => { return b.Col3 - a.Col3 });
+      this.totti = totti;
+
+      let muriel = [];
+      for (let i = squadre_con_statistica.length - 1; i >= 0; i--) {
+        let s = squadre_con_statistica[i];
+        muriel.push({
+          'Id': s.Id,
+          'Name': s.Name,
+          'Coach': s.Coach,
+          'Jersey': s.Jersey,
+          'Col1': s.Titolari.reduce((partialSum, x) => partialSum + x, 0),
+          'Col2': s.Panca.reduce((partialSum, x) => partialSum + x, 0),
+          'Col3': (s.Titolari.reduce((partialSum, x) => partialSum + x, 0) + s.Panca.reduce((partialSum, x) => partialSum + x, 0)) == 0 ? 0 : Math.round(100 * (s.Panca.reduce((partialSum, x) => partialSum + x, 0) / (s.Titolari.reduce((partialSum, x) => partialSum + x, 0) + s.Panca.reduce((partialSum, x) => partialSum + x, 0))), 1)
+        })
+      }
+      muriel.sort((a, b) => { return b.Col3 - a.Col3 });
+      for (var i = 0; i < muriel.length; i++) {
+        muriel[i].Col3 = muriel[i].Col3 + '%'
+      }
+      this.muriel = muriel;
 
       this.to_load = 'Completato';
     }
---
+++ src/views/components/LorenzoSpecialPrize.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="card">
+    <div class="p-3 pb-0 card-header">
+      <div class="mb-1 row align-items-center">
+        <img :src="Image" style="height:220px" />
+        <h6 class="mb-0 text-sm">{{Title}}</h6>
+        <p class="text-xs text-secondary mb-2">{{Subtitle}}</p>
+      </div>
+    </div>
+    <!-- https://c.tenor.com/4Zl464BlTSUAAAAM/football-haiti.gif --> 
+    <div class="pb-0 pt-2 card-body">
+      <LorenzoImageText 
+      :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + dataset[0].Jersey"
+      :text="dataset[0].Name" 
+      :secondary_text="dataset[0].Coach"
+      />
+      <div class="mb-1 mt-3 row align-items-center">
+        <div class="table-responsive">
+          <table class="table align-items-center">
+            <thead>
+              <th></th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i :class="'fas ' + Col1.Icon" :style="'color: ' + Col1.Icon_Color"></i>{{ Col1.Name }}</th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0"><i :class="'fas ' + Col2.Icon" :style="'color: ' + Col2.Icon_Color"></i>{{ Col2.Name }}</th>
+              <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 p-0">{{ Col3.Name }}</th>
+            </thead>
+            <tbody>
+              <tr v-for="(squadra, index) in dataset" :key="index" >
+                <td style="padding: 0rem 0.5rem !important">
+                  <LorenzoImageText 
+                  :image="'https://d2lhpso9w1g8dk.cloudfront.net/web/risorse/maglietta_2022/' + squadra.Jersey"
+                  />
+                </td>
+                <td style="padding: 0rem 0.5rem !important"> {{ squadra.Col1 }} </td>
+                <td style="padding: 0rem 0.5rem !important"> {{ squadra.Col2 }} </td>
+                <td style="padding: 0rem 0.5rem !important"> {{ squadra.Col3 }} </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import LorenzoImageText from "@/views/components/LorenzoImageText.vue";
+
+  export default {
+    name: "lorenzo-special-prize",
+    components: {
+      LorenzoImageText
+    },
+    props: {
+      dataset: {},
+      Title: String,
+      Subtitle: String,
+      Image: String,
+      Col1: Object,
+      Col2: Object,
+      Col3: Object,
+    }
+  };
+</script>
