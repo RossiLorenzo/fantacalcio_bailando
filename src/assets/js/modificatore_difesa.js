0544164dd55c564ad2947518ccd48aa83097b64c
Lorenzo Rossi <lorenzorossi@fb.com>
2023-01-28 18:54:07 +0000

    update code

--- src/assets/js/modificatore_difesa.js
+++ src/assets/js/modificatore_difesa.js
@@ -6,15 +6,19 @@ export default function mod_difesa(t){
 		if (v == 55) { return 4 }
 		return v;
 	}
+	//console.log(t);
 	let difesa_all = t.filter(x => x.r == 'D');
 	if (difesa_all.length < 4) {
 		return 0;
 	}
-	let difesa = t.filter(x => x.r == 'D').map(x => remove_nulls(x.voto_iniziale)).sort().reverse().slice(0, 3);
+	let difesa = t.filter(x => x.r == 'D').map(x => remove_nulls(x.voto_finale)).sort().reverse().slice(0, 3);
+	//console.log(difesa);
 
 	
-	let portiere = t.filter(x => x.r == 'P').map(x => remove_nulls(x.voto_iniziale))[0];
+	let portiere = t.filter(x => x.r == 'P').map(x => remove_nulls(x.voto_finale))[0];
+	//console.log(portiere);
 	let media_difesa = difesa.reduce((partialSum, x) => partialSum + x, 0) + portiere;
+	//console.log(media_difesa)
 	let mod_difesa_punti;
 	if (media_difesa >= 28) {
 		mod_difesa_punti = 5
