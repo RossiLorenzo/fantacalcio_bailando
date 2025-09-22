a9cfba25da1d4cf5bdbb25f2679fae7f0197d77c
Lorenzo Rossi <lorenzorossi@meta.com>
2025-09-22 11:20:56 +0100

    update code

--- src/assets/js/fantacalcio_apis.js
+++ src/assets/js/fantacalcio_apis.js
@@ -16,7 +16,7 @@ export default async function fantacalcio_apis(resource, params = new Map()){
 	// Headers used in the requests
 	let overall_headers = {
 		'Content-Type': 'application/json',
-		'app_key': '1a2b3c4d5e6f7a8b9c0d1e2f3a4b5c6d7e8f9a0',
+		'app_key': 'QCLzFqW0jqUgGdJePaj50nEAkMHeXfL2',
 		'lega_token': Cookies.get('lega_token'),
 		'user_token': Cookies.get('utente_token')
     };
